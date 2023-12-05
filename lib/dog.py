63a9524d0efaf4010e43ea20cc2e3a9de2b599a7
Matthew Bomett <bomettmat2@gmail.com>
2023-12-05 19:54:06 +0300

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,37 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    def __init__(self, name, breed):
+# initializes a Dog object with specified name and breed
+        self.name = name
+        self.breed = breed
+
+
+    @property
+    def name(self):
+# retrieves the name of the dog
+        return self.name
+    
+    @name.setter
+    def name(self, value):
+# sets the name of the dog
+        if not isinstance(value, str) or len(value) < 1 or len(value) > 25:
+# validates the condition
+            print("Name must be string between 1 and 25 characters.")
+            return
+        self._name = value
+
+    @property
+    def breed(self):
+# retrieves the breed of the dog
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+# sets the breed of the dog
+        if value not in APPROVED_BREEDS:
+# validates the condition
+            print("Breed must be in list of approved breeds.")
+            return
+        self._breed = value
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self, name, job):
+# initializes a Person object with specified name and job
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+# retrieves the name of the person
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+# sets the name of the person
+        if not isinstance(value, str) or len(value) > 25:
+# validates the condition
+            print("Name must be string under 25 characters.")
+            return
+        self._name = value.title()
+
+    @property
+    def job(self):
+# retrieves the job of the person
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+# sets the job of the person
+        if value not in APPROVED_JOBS:
+# validates the condition
+            print("Job must be in list of approved jobs.")
+            return
+        self._job = value
     pass
